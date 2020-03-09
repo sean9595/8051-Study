778c45ecf511843f582242bb0880de124c932c5e
sean9595 <39852892+sean9595@users.noreply.github.com>
2020-03-10 01:16:33 +0900

    #5 02-2
    02-2

--- at89s52_study_01/02-1/main.c
+++ at89s52_study_01/02-1/main.c
@@ -1,55 +1,62 @@
 /*
-*Project name: External interrupt
+*Project name: Use timer interrupt to control LEDs.
 *File name: main.c
 *Author: Sean Kim
-*Date: 02.28.20
-*Edited: 03.03.20
+*Date: 03.08.20
+*Edited: 
 */
 
 #include "main.h"
 #include "at89x52.h"
-#include "essen_prog.h"
 
-bit flag_interrupt_01 = 0; //'sbit' is used when we name some bit value on SFR. 
-unsigned char led;
 
-void delay(unsigned int ms);
-
+unsigned char LED, count;
+bit T_int_flag = 0;
 
 void main()
 {
-    TCON = 0x01; //Timer control register. 
-	               //0x00000000 => reset value
-	               //IT0 reg. = 1 -> Set external interrupt 0. 
-	               //IE0 reg. -> When external interrupt occurs, this flag sets as 1. This makes the system jumps to the ISR.
+	/*Basic setting for timer interrupt*/
+	TMOD = 0x01; //GATE = 0, C/T = 0, Timer/counter mode 1
+	TF0 = 0; // Timer/counter 0 interrupt flag clear
+	TL0 = 0x82 ;
+	TH0 = 0x35 ; //Time const. for 0.02s. // 0x8235*0.6us is approximately equal to 0.02s.
+	IE = 0x82; // IE -> interrupt enable reg. Timer interrupt enables & entire interrupt enable.
+	IP = 0x02; // IP -> interrupt priority. Set timer interrupt 0 as the highest priority. 
+	           // Should have to consider which register(btw IE & IP) comes first. 
+	
+	TR0 = 1; // Timer interrupt 0 RUN.
+	
+	/*Code for set led*/
+	LED = 0xfe; // 0b11111110
+	P1 = LED;
+	
+	count = 50; // 0.02*50 = 1sec.
+	
+	/*Infinite loop*/
+	while(1) // Stay for interrupt
+	{
+		if(T_int_flag)
+		{
+			LED = (LED<<1) | 0x01; // 1 bit shifting
+			
+			if (LED == 0xff) LED = 0xfe;
+			P1 = LED;
+			
+			count = 50; // Counter reset.
+			T_int_flag = 0; // Flag reset.
+		}
+	}
 
-    delay(1000); 
-    
-    EX0 = 1; //External interrupt '0' enable.
-    EA= 1; //This bit is for athorizing interrupt. Has to put this code back. 
-    PX0 = 1; //
-    led = 0xfe;
-    P1 = led;
-
-    while(1)
-    {
-        if(flag_interrupt_01)
-        {
-        EA = 0;
-        led = (led<<1) | 0x01;
-
-        if(led == 0xff) led = 0xfe;
-
-        P1 = led;
-        delay(20); //Delay for 20ms. This is for debouncing.
-        EA = 1;
-        flag_interrupt_01 = 0;
-        }
-    }
 }
 
-void EXT0_int() interrupt 0
+void T_int() interrupt 1 // void [user name]() interrupt [vector num.]
 {
-    flag_interrupt_01 = 1; //Don't let the ISR code unnecessarily long. 
+	count--; // Since the const. count is 50, T_int_flag is set until count decreased to zero. 
+	         // This operation delays LED shifting 1sec.
+	if(!count) T_int_flag = 1;
+	
+	TL0 = 0x82 ;
+	TH0 = 0x35 ; // Reset of these two register is necessary for periodic interrupt since it is mode 1.
+	EA = 1; // Interrupt enables
+	return;
 }
-
