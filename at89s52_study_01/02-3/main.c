10bfe6fccf4a10ce9698c0462632710c0c900f83
sean9595 <39852892+sean9595@users.noreply.github.com>
2020-03-11 22:55:35 +0900

    #6 02-3
    02-3 code generation

---
+++ at89s52_study_01/02-3/main.c
@@ -0,0 +1,57 @@
+/*
+*Project name: Serial communication 01 (without interrupt)
+*File name: main.h
+*Author: Sean Kim
+*Date: 03.11.20
+*Edited: 03.11.20
+*/
+
+#include "main.h"
+#include "at89x52.h"
+#include "essen_prog.h"
+
+#define LF 0x0a; // ASCII CODE LF 
+#define CR 0x0d; // ASCII CODE CR
+
+void main()
+{
+	unsigned int i;
+	
+	/*Baudrate generation*/
+	TMOD = 0x20;//T/C mode 2, timer 1
+	TH1 = 0xfb; // (256-TH1)={(20*10^6)*(2^SMOD value)}/(32*12*9600) for baudrate 9600bps
+	
+	/*Serial communication setting*/
+	PCON = 0x00; //SMOD=0
+	TR1 = 1; //Timer 1 running.
+	SCON = 0x40; // 0b0100\0000 -> Serial mode 1, SM2=0, REN=0(only transmitting)
+
+	/*Infinite loop*/
+	while(1)
+	{
+		for(i=0;i<=25;i++) // a~z
+		{
+			SBUF = 'a' + i;
+			delay(50); //Delay for 0.05s
+		}
+		
+		SBUF = LF; 
+		delay(50);
+		
+		SBUF = CR;
+		delay(50);
+		
+		for(i=0;i<=25;i++) // A~Z
+    {
+			SBUF = 'A' + i;
+			delay(50);
+		}
+	  SBUF = LF;
+		delay(50);
+		
+		SBUF = CR;
+		delay(50);
+		
+		delay(500);
+	}
+}
