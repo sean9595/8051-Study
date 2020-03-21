f12e619d586316a2944743fe1da58dfc1fa03ca0
sean9595 <39852892+sean9595@users.noreply.github.com>
2020-03-21 16:06:04 +0900

    #10 4_digit timer contorl
    Final code

--- at89s52_study_01/03-1-2/main.c
+++ at89s52_study_01/03-1-2/main.c
@@ -1,9 +1,9 @@
 /*
-*Project name: 4_digit 7-segment timer (with two timer interrupts)                                                                                                                                                                                                                                                                  
+*Project name: 4_digit 7-segment timer                                                                                                                                                                                                                                                                  
 *File name: main.c
 *Author: Sean Kim
 *Date: 03.19.20
-*Edited: 
+*Edited: 03.20.20
 */
 
 #include "main.h"
@@ -15,39 +15,39 @@ bit T_int_flag_02 = 0;
 
 
 unsigned char a=0, b=0, c=0, d=0;
-unsigned char i=0, j=0, k=0;
-unsigned char num[] = {0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x02, 0x78, 0x00, 0x10}; 
-                          //7-segment common cathod
+
+unsigned char num[] = {0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x02, 0x78, 0x00, 0x10};
+                          //7-segment common cathod.
+                          //Use PNP TR for suppling proper amount of current.
               
 unsigned char count;
-unsigned char count_1;
+unsigned char count_01=0; //This code is for displaying each digit st the same time.
 	
 void display()
 	{
-		if(count_1 == 1)
+		if(count_01 == 0)
 		{
 		P0 = 0x0e;
 		P1 = num[a];
 		}
 		
-		if(count_1 == 2)
+		else if(count_01 == 1)
 		{
 		P0 = 0x0d;
 		P1 = num[b];
 		}
 		
-		if(count_1 == 3)
+		else if(count_01 == 2)
 		{
 		P0 = 0x0b;
 		P1 = num[c];
 		}
 		
-		if(count_1 == 4)
+		else if(count_01 == 3)
 		{
 		P0 = 0x07;
 		P1 = num[d];
-			
-		count_1 = 0;
+		
 		}
 		
 		return;
@@ -60,8 +60,9 @@ void main()
 	TF0 = 0; // Timer/counter 0 interrupt flag clear
 
 	
-	TL0 = 0xc3 ;
-	TH0 = 0x50 ; // TI0 occurs every 0.05 sec 
+	TH0 = 0xec ;
+	TL0 = 0x78 ; // TI0 occurs every 5 ms 
+	             // 1us/MC -> (0xffff-0xec78) = 5,000
 	
 	IE = 0x82; // 0b1000 0010 IE -> interrupt enable reg. Timer interrupt enables & entire interrupt enable.
    
@@ -71,57 +72,44 @@ void main()
 	
 	TR0 = 1; // Timer interrupt 0 RUN.
 	
-	count = 20; // 0.05*20 = 1sec. 
-	count_1 = 0;
+	count = 200; // 0.005*200 = 1sec. 
+
 	
 	while(1)
 	{
 		if(T_int_flag_01)
 		{
-			j++;
-			k++;
 			
-			if(i==10)
-			{
-				a++;
-				
-			if(a>9)	a=0;
+		d++;
+			
+      if (d > 9) 
+		  {
+       d = 0;
+       c++; // 0009 -> 0010, 0019 -> 0020, ...
+        
+				if (c > 9)
+				{
+         c = 0;
+         b++;
 					
-				i=0;
-			}
-			
-			if(j==100)
-			{
-				b++;
-				
-			if(b>9)	b=0;
-			  i++;	
-				j=0;
-			}
-			
-			if(k==10)
-			{
-				c++;
-					
-			if(c>9)	c=0;
-					
-				k=0;
-			}
-			
-				d++;
-				
-			if(d>9)	d=0;
-		
-			
-			count = 20;
+          if (b > 9) 
+					{
+            b = 0;
+            a++;
+						
+            if (a > 9) 
+						{
+                a = 0;
+            }
+          }
+        }
+      }
+			count = 200;
 		  T_int_flag_01 = 0;
 		}
 		
-		//else if(T_int_flag_02)
-		  count_1++;
-		
 			display();
-		
+ 
 	}
 }
 
@@ -131,9 +119,12 @@ void T_int_01() interrupt 1 // void [user name]() interrupt [vector num.]
 	count--; // Since the const. count is 50, T_int_flag is set until count decreased to zero. 
 	         
 	if(!count) T_int_flag_01 = 1;
+
+  count_01++;
+	if(count_01>3) count_01 = 0; //count increment & count initialization have to be placed at the same place.
 	
-	TL0 = 0xc3 ;
-	TH0 = 0x50 ; // Reset of these two register is necessary for periodic interrupt since it is mode 1.
+	TH0 = 0xec ;
+	TL0 = 0x78 ;// Reset of these two register is necessary for pe riodic interrupt since it is mode 1.
 	EA = 1; // Interrupt enables
 	return;
 }
